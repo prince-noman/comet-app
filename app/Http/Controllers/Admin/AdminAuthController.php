c64a7f0d53e05528f9c7fda743efab1f2b027651
prince-noman <prince1000bd@gmail.com>
2022-08-05 23:01:03 +0600

    Cell and Useraname Login Done

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -26,7 +26,7 @@ class AdminAuthController extends Controller {
         ] );
 
 //Try to login
-        if ( Auth::guard( 'admin' )->attempt( ['email' => $request->auth, 'password' => $request->password] ) ) {
+        if ( Auth::guard( 'admin' )->attempt( ['email' => $request->auth, 'password' => $request->password] ) || Auth::guard( 'admin' )->attempt( ['cell' => $request->auth, 'password' => $request->password] ) || Auth::guard( 'admin' )->attempt( ['username' => $request->auth, 'password' => $request->password] ) ) {
             return redirect()->route( 'admin.dashboard' );
         } else {
             return redirect()->route( 'admin.login' )->with( 'warning', 'Incorrect Credentials.' );
